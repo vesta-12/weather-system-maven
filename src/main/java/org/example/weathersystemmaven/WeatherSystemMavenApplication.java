6ca3dcd440f3f26b9eb221060ff6aff72e5e6d3b
vesta-12 <sofiya.serbina@narxoz.kz>
2026-04-06 23:49:43 +0500

    add dependencies

--- src/main/java/org/example/weathersystemmaven/WeatherSystemMavenApplication.java
+++ src/main/java/org/example/weathersystemmaven/WeatherSystemMavenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherSystemMavenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherSystemMavenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSystemMavenApplication.class, args);
+    }
 
 }
