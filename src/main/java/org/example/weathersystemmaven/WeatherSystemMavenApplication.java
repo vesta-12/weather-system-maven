012de77b8dfcd901c2736c6cf1240fbfc15491d7
vesta-12 <sofiya.serbina@narxoz.kz>
2026-04-06 23:47:17 +0500

    read me

---
+++ src/main/java/org/example/weathersystemmaven/WeatherSystemMavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.weathersystemmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSystemMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSystemMavenApplication.class, args);
+	}
+
+}
